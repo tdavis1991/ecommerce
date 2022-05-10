616f3f9b3b2cdba2a6aaec04eb3e50a8fcadc076
Tevin_Davis <tdavis.070991@gmail.com>
2022-05-10 19:46:52 -0400

    Added Rights and icons to footer component

--- ecommerce/components/Footer.jsx
+++ ecommerce/components/Footer.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter, AiFillFacebook } from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>2022 Frost Headphones All rights reserved</p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+        <AiFillFacebook />
+      </p>
+    </div>
   )
 }
 
