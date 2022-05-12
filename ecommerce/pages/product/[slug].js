5e091206e84746debb3c08d0ec5d5d46efebcb6c
Tevin_Davis <tdavis.070991@gmail.com>
2022-05-11 20:13:07 -0400

    Created details page for products

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -1,16 +1,70 @@
-import React from 'react';
+import React,  { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 
 import { client, urlFor } from '../../lib/client';
+import { Product } from '../../components';
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, price, details } = product;
+
+    const [index, setIndex] = useState(0);
 
     return (
         <div>
             <div className='product-details-container'>
                 <div>
-                    <div className='image-container'>
-                        <img src={urlFor(image && image[0])} />
+                    <div className="image-container">
+                        <img src={urlFor(image && image[index])} className="product-detail-image" />
+                    </div>
+                    <div className="small-images-container">
+                        {image?.map((item, i) => (
+                        <img 
+                            key={i}
+                            src={urlFor(item)}
+                            className={i === index ? 'small-image selected-image' : 'small-image'}
+                            onMouseEnter={() => setIndex(i)}
+                        />
+                        ))}
+                    </div>
+                </div>
+                <div className='product-detail-desc'>
+                    <h1>{name}</h1>
+                    <div className='reviews'>
+                        <div>
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                        </div>
+                        <p>
+                            (20)
+                        </p>
+                    </div>
+                    <h4>Details:</h4>
+                    <p>{details}</p>
+                    <p className='price'>${price}</p>
+                    <div className='quantity'>
+                        <h3>Quanity:</h3>
+                        <p className='quantity-desc'>
+                            <span className='minus' onClick=''><AiOutlineMinus /></span>
+                            <span className='num' onClick=''>0</span>
+                            <span className='plus' onClick=''><AiOutlinePlus /></span>
+                        </p>
+                    </div>
+                    <div className='buttons'>
+                        <button type='button' className='add-to-cart' onClick=''>Add to Cart</button>
+                        <button type='button' className='buy-now' onClick=''>Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div className='maylike-products-wrapper'>
+                <h2>You may also like</h2>
+                <div className='marquee'>
+                    <div className='maylike-products-container track'>
+                        {products.map((item) => (
+                            <Product key={item._id} product={item}/>
+                        ))}
                     </div>
                 </div>
             </div>
@@ -19,7 +73,7 @@ const ProductDetails = ({ product, products }) => {
 }
 
 export const getStaticPaths = async () => {
-    const query = `*[_typpe == product]{
+    const query = `*[_type == product]{
         slug {
             current
         }
@@ -54,3 +108,4 @@ export const getStaticProps = async ({ params: { slug }}) => {
 }
 
 export default ProductDetails;
+
