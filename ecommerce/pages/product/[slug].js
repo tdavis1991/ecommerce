1da236357878cc61e45c681203965f9cf03d476b
Tevin_Davis <tdavis.070991@gmail.com>
2022-05-11 20:50:04 -0400

    Working on connecting app to context

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -3,11 +3,13 @@ import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-
 
 import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, price, details } = product;
 
     const [index, setIndex] = useState(0);
+    const { decQty, incQty, qty } = useStateContext();
 
     return (
         <div>
@@ -47,9 +49,9 @@ const ProductDetails = ({ product, products }) => {
                     <div className='quantity'>
                         <h3>Quanity:</h3>
                         <p className='quantity-desc'>
-                            <span className='minus' onClick=''><AiOutlineMinus /></span>
-                            <span className='num' onClick=''>0</span>
-                            <span className='plus' onClick=''><AiOutlinePlus /></span>
+                            <span className='minus' onClick={decQty}><AiOutlineMinus /></span>
+                            <span className='num' onClick=''>{qty}</span>
+                            <span className='plus' onClick={incQty}><AiOutlinePlus /></span>
                         </p>
                     </div>
                     <div className='buttons'>
